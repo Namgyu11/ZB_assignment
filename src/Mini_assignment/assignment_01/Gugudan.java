cd31f51080036ea1b8bd1d5b40a538c717c53ca3
Namgyu11 <hagaji3@naver.com>
2023-08-02 17:01:04 +0900

    구구단 프로그램

--- src/Mini_assignment/assignment_01/Gugudan.java
+++ src/Mini_assignment/assignment_01/Gugudan.java
@@ -1,2 +1,21 @@
-package Mini_assignment.assignment_01;public class Gugudan {
+package Mini_assignment.assignment_01;
+
+/*
+ 백엔드 스쿨 16기
+ 이름: 하남규
+ */
+public class Gugudan {
+    public static void main(String[] args) {
+
+        System.out.println("[구구단 출력]");
+        for(int i = 1; i < 10; i++){
+            for(int j = 2; j < 10; j++){
+                System.out.print(String.format("%02d X ",j));
+                System.out.print(String.format("%02d = ",i));
+                System.out.print(String.format("%02d\t",j*i));
+            }
+                System.out.println();
+        }
+
+    }
 }
